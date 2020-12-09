ca15ff94e3bbbb6116960c0a4c4ba6ec58284061
amangupta37 <amangupta6121998@gmail.com>
2020-12-09 12:36:02 +0530

    added api for image search

---
+++ app.js
@@ -0,0 +1,29 @@
+const btn = document.getElementById("clk");
+const input = document.getElementById("inpt-box");
+const output = document.getElementById("gallery-box");
+
+const url = "https://api.unsplash.com/search/photos?query=";
+
+
+
+
+function recivedata(datain){
+    return url + datain + "&client_id=1M2Bjw3fQ9ZcA8inqDboIgXzHhHL2CqsELWniCElfbQ";
+}
+
+ btn.addEventListener("click", function(){
+    console.log("one click");
+   console.log(input.value);
+   const inputdata = input.value;
+    // output.innerText=input.value;
+ 
+     fetch(recivedata(inputdata))
+     .then(response => response.json())
+     .then(json => {
+      json.results.forEach(photo => {
+
+        document.getElementById("newpic").src=photo.urls.regular;
+         
+      });
+   })
+ });
