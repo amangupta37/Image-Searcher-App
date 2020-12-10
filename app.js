bca163aef6de407538adc3408b3ff7f74519b56c
amangupta37 <amangupta6121998@gmail.com>
2020-12-10 11:08:40 +0530

    improved js file for multiple images fetch

--- app.js
+++ app.js
@@ -1,30 +1,47 @@
 const btn = document.getElementById("clk");
 const input = document.getElementById("inpt-box");
-const output = document.getElementById("box");
+//const output = document.getElementById("box");
+const wmsg= document.getElementById("wait-msg")
+const reload=document.getElementById("load-size");
 
 const url = "https://api.unsplash.com/search/photos?query=";
 
 
-
-
 function recivedata(datain){
-    return url + datain + "&client_id=1M2Bjw3fQ9ZcA8inqDboIgXzHhHL2CqsELWniCElfbQ";
+    return url + datain + "&client_id=1M2Bjw3fQ9ZcA8inqDboIgXzHhHL2CqsELWniCElfbQ&per_page=30";
 }
 
  btn.addEventListener("click", function(){
+
     console.log("one click");
    console.log(input.value);
+
+  
    const inputdata = input.value;
     // output.innerText=input.value;
- 
+    if(inputdata!="")
+    {
+    wmsg.innerText="Wait Fetching your result it will display here 👇  !!!";
+    }
+    else
+    {
+      alert("enter data");
+     
+    }
      fetch(recivedata(inputdata))
      .then(response => response.json())
      .then(json => {
       json.results.forEach(photo => {
-        
-        output.innerHTML = `<img src=${photo.urls.regular} alt="cat" />`
+       
+     let newres = `<img src="${photo.urls.regular}">`;
+     $("#box").append(newres);
 
-      });
-   })
+   
+   });
+ });
+
+ });
+
+ reload.addEventListener("click", function(){
+  location.reload();
  })
-
